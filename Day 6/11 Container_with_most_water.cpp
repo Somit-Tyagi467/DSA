177b916f27f7c5e46c80f1cf75bd875321e32e7a
Somit Tyagi <somittyagi559@gmail.com>
2026-03-18 23:23:05 +0530

    Revision day 2

--- Day 6/11 Container_with_most_water.cpp
+++ Day 6/11 Container_with_most_water.cpp
@@ -17,3 +17,23 @@ public:
         return max_area;
     }
 };
+//revision
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int n= height.size();
+        int l=0;
+        int r=n-1;
+        int max_area=0;
+        while(l<r){
+            max_area=max(max_area,min(height[l],height[r])*(r-l));
+            if(height[l]<height[r]){
+                l++;
+            }
+            else{
+                r--;
+            }
+        }
+        return max_area;
+    }
+};
