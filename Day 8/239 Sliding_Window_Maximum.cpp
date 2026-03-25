1b86ebf3b16a374ff31730be9e5559bfa60778f6
Somit Tyagi <somittyagi559@gmail.com>
2026-03-25 23:47:39 +0530

    revision

--- Day 8/239 Sliding_Window_Maximum.cpp
+++ Day 8/239 Sliding_Window_Maximum.cpp
@@ -16,3 +16,22 @@ public:
     return arr;
     }
 };
+//revision
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> arr;
+        int left=0;
+        vector<int>::iterator index;
+        for(int right=0;right<nums.size();right++){
+            if((right-left)+1==k){
+                auto start = nums.begin()+left;
+                auto end = nums.begin()+right+1;
+                index = max_element(start,end);
+                arr.push_back(*index);
+                left++;
+            }
+        }
+    return arr;
+    }
+};
